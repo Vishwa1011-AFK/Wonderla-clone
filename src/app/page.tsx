86038cedbed76bfd2a3e1cc2314178a756d9f552
Vishwa1011 <imt_2022119@iiitm.ac.in>
2025-05-12 16:15:23 +0530

    Initialised Next App and added some data from Kochi and basic RideSection and RideCard setup done

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import RidesSection from '@/components/RidesSection';
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Wonderla_frontend</h1>
+      <RidesSection />
+    </main>
+  );
+}
